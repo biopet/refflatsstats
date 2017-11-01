90e6df8a3e5efffe12c38b79fccd05c1d0836d5c
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-11-01 13:57:25 +0100

    added options descriptions

--- src/main/scala/nl/biopet/tools/refflatsstats/ArgsParser.scala
+++ src/main/scala/nl/biopet/tools/refflatsstats/ArgsParser.scala
@@ -8,25 +8,25 @@ class ArgsParser(cmdName: String) extends AbstractOptParser[Args](cmdName) {
   opt[File]('a', "annotationRefflat") required () maxOccurs 1 valueName "<file>" action {
     (x, c) =>
       c.copy(refflatFile = x)
-  }
+  } text "The refflat file used for annotation"
   opt[File]('R', "referenceFasta") required () maxOccurs 1 valueName "<file>" action {
     (x, c) =>
       c.copy(referenceFasta = x)
-  }
+  } text "The reference fasta file"
   opt[File]('g', "geneOutput") required () maxOccurs 1 valueName "<file>" action {
     (x, c) =>
       c.copy(geneOutput = x)
-  }
+  } text "Ouput file for genes"
   opt[File]('t', "transcriptOutput") required () maxOccurs 1 valueName "<file>" action {
     (x, c) =>
       c.copy(transcriptOutput = x)
-  }
+  } text "Output file for transcripts"
   opt[File]('e', "exonOutput") required () maxOccurs 1 valueName "<file>" action {
     (x, c) =>
       c.copy(exonOutput = x)
-  }
+  } text "Output file for exons"
   opt[File]('i', "intronOutput") required () maxOccurs 1 valueName "<file>" action {
     (x, c) =>
       c.copy(intronOutput = x)
-  }
+  } text "Output file for introns"
 }
